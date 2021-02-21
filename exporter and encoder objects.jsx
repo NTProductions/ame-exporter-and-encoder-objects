8f6ab54a87acaa81310b27141deeca938dc2d7cd
Nate Lovell <Natelovell99@gmail.com>
2021-02-21 15:15:54 -0700

    Create exporter and encoder objects.jsx

---
+++ exporter and encoder objects.jsx
@@ -0,0 +1,27 @@
+﻿// ame exporter and encoder objects
+
+// exporter
+var exporter = app.getExporter();
+// attributes
+exporter.encodeId;
+exporter.result;
+exporter.elapsedMilliseconds;
+// methods
+exporter.removeAllBatchItems();
+exporter.trimExportForSR();
+exporter.exportItem();
+exporter.exportGroup();
+
+// encoder (host)
+var encoder = app.getEncoderHost();
+// methods
+encoder.runBatch();
+encoder.pauseBatch();
+encoder.stopBatch();
+encoder.isBatchRunning();
+encoder.getFormatList();
+encoder.createEncoderForFormat();
+enoder.createMediaComparator();
+encoder.getSupportedImportFileTypes();
+encoder.getSourceInfo();
+encoder.getCurrentBatchPreview();
